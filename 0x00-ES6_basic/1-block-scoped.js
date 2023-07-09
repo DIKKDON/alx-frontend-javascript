a0e732414b002ad4000eb935858c3bbcff16c17b
DIKKDON <dicksondhideraudeh@gmail.com>
2023-07-09 22:53:03 +0000

    task0

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,4 +1,5 @@
-export default function taskBlock(trueOrFalse) {
+}
+rt default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
@@ -6,5 +7,5 @@ export default function taskBlock(trueOrFalse) {
     const task = true; // eslint-disable-line no-unused-vars
     const task2 = false; // eslint-disable-line no-unused-vars
   }
-}
+
   return
